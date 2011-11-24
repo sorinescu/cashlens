aff3107a22aade3768c65e28cbea10d79cd95a80
Sorin Otescu <sorinu@gmail.com>
2011-11-24 14:10:04 +0200

    Added Eclipse-generated files.

---
+++ src/com/udesign/cashlens/CashLensActivity.java
@@ -0,0 +1,13 @@
+package com.udesign.cashlens;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CashLensActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
