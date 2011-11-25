ee6b10e5c22a4f42be76bfb1c6c0ed98474e4ebd
Sorin Otescu <sorinu@gmail.com>
2011-11-25 12:22:48 +0200

    Initial commit for AddExpense.

---
+++ src/com/udesign/cashlens/AddExpenseActivity.java
@@ -0,0 +1,17 @@
+package com.udesign.cashlens;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AddExpenseActivity extends Activity {
+
+	/* (non-Javadoc)
+	 * @see android.app.Activity#onCreate(android.os.Bundle)
+	 */
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+
+}
