0491ae2bec4b62021c4b5ff8fcabea5ee40a7ac5
Sorin Otescu <sorinu@gmail.com>
2011-11-25 19:57:47 +0200

    Implemented transparent numeric pad and number input.

--- src/com/udesign/cashlens/AddExpenseActivity.java
+++ src/com/udesign/cashlens/AddExpenseActivity.java
@@ -1,17 +1,201 @@
 package com.udesign.cashlens;
 
+import java.io.IOException;
+
 import android.app.Activity;
+import android.graphics.Color;
+import android.hardware.Camera;
 import android.os.Bundle;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
-public class AddExpenseActivity extends Activity {
-
+public class AddExpenseActivity extends Activity implements SurfaceHolder.Callback 
+{
+	 
+	private RelativeLayout mLayout;
+	private SurfaceView mCameraPreview;
+	private android.hardware.Camera mCamera;
+	private boolean mInPreview;
+	private Button mNumButtons[];
+	private Button mDelButton;
+	private Button mDotButton;
+	private TextView mExpenseText;
+	public String mExpenseInt;
+	public String mExpenseFrac;
+	public boolean mExpenseDot;
+	
 	/* (non-Javadoc)
 	 * @see android.app.Activity#onCreate(android.os.Bundle)
 	 */
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
+	protected void onCreate(Bundle savedInstanceState) 
+	{
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.add_expense);
+		
+		mExpenseInt = "";
+		mExpenseFrac = "";
+		mExpenseDot = false;
+		
+		mLayout = (RelativeLayout)findViewById(R.id.addExpense);
+		mLayout.setBackgroundColor(Color.argb(0, 0, 0, 0));    	
+
+		mCameraPreview = (SurfaceView)findViewById(R.id.cameraPreview);
+		mInPreview = false;
+
+		mExpenseText = (TextView)findViewById(R.id.txtSum);
+		updateExpenseText();
+		
+		mNumButtons = new Button[10];
+		mNumButtons[0] = (Button)findViewById(R.id.btn0);
+		mNumButtons[1] = (Button)findViewById(R.id.btn1);
+		mNumButtons[2] = (Button)findViewById(R.id.btn2);
+		mNumButtons[3] = (Button)findViewById(R.id.btn3);
+		mNumButtons[4] = (Button)findViewById(R.id.btn4);
+		mNumButtons[5] = (Button)findViewById(R.id.btn5);
+		mNumButtons[6] = (Button)findViewById(R.id.btn6);
+		mNumButtons[7] = (Button)findViewById(R.id.btn7);
+		mNumButtons[8] = (Button)findViewById(R.id.btn8);
+		mNumButtons[9] = (Button)findViewById(R.id.btn9);
+		
+		mDelButton = (Button)findViewById(R.id.btnDel);
+		mDotButton = (Button)findViewById(R.id.btnDot);
+		
+		SurfaceHolder holder = mCameraPreview.getHolder();
+		holder.addCallback(this);
+		holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+		
+		for (int i=0; i<10; ++i)
+			mNumButtons[i].setOnClickListener(new NumericButtonClickListener(this, i));
+		
+		mDelButton.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) 
+			{
+				AddExpenseActivity parent = AddExpenseActivity.this;
+				
+				if (parent.mExpenseFrac.length() > 0)
+					parent.mExpenseFrac = parent.mExpenseFrac.substring(0, parent.mExpenseFrac.length() - 1); 
+				else
+				{
+					parent.mExpenseDot = false;
+					
+					if (parent.mExpenseInt.length() > 0)
+						parent.mExpenseInt = parent.mExpenseInt.substring(0, parent.mExpenseInt.length() - 1); 
+				}
+				
+				parent.updateExpenseText();
+			}
+		});
+		
+		mDotButton.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) 
+			{
+				AddExpenseActivity parent = AddExpenseActivity.this;
+				
+				parent.mExpenseDot = true;
+				parent.updateExpenseText();
+			}
+		});
 	}
 
+	public void updateExpenseText()
+	{
+		String txt;
+		
+		if (mExpenseInt.length() > 0)
+			txt = mExpenseInt;
+		else
+			txt = "0";
+		
+		if (mExpenseDot && mExpenseFrac.length() > 0)
+			txt += "." + mExpenseFrac;
+		
+		mExpenseText.setText(txt);
+	}
+	
+	/* (non-Javadoc)
+	 * @see android.app.Activity#onDestroy()
+	 */
+	@Override
+	protected void onDestroy() 
+	{
+		if (mCamera != null)
+		{
+			if (mInPreview)
+				mCamera.stopPreview();
+	        mCamera.release();
+	        mCamera = null;
+		}
+
+		super.onDestroy();
+	}
+
+	public void surfaceChanged(SurfaceHolder holder, int format, int width,
+			int height) 
+	{
+        // Now that the size is known, set up the camera parameters and begin
+        // the preview.
+        Camera.Parameters parameters = mCamera.getParameters();
+        parameters.setPreviewSize(width, height);
+        mCamera.setParameters(parameters);
+        mCamera.startPreview();
+        mInPreview = true;
+	}
+
+	public void surfaceCreated(SurfaceHolder holder) 
+	{
+        // The Surface has been created, acquire the camera and tell it where
+        // to draw.
+        mCamera = Camera.open();
+        try 
+        {
+           mCamera.setPreviewDisplay(holder);
+        } 
+        catch (IOException exception) 
+        {
+            mCamera.release();
+            mCamera = null;
+        }
+	}
+
+	public void surfaceDestroyed(SurfaceHolder holder) 
+	{
+        // Surface will be destroyed when we return, so stop the preview.
+        // Because the CameraDevice object is not a shared resource, it's very
+        // important to release it when the activity is paused.
+        if (mCamera != null)
+        {
+			if (mInPreview)
+	        	mCamera.stopPreview();
+	        mCamera.release();
+	        mCamera = null;
+        }
+	}
+}
+
+class NumericButtonClickListener implements OnClickListener
+{
+	private int mFigure;
+	private AddExpenseActivity mParent;
+	
+	NumericButtonClickListener(AddExpenseActivity parent, int figure)
+	{
+		mParent = parent;
+		mFigure = figure;
+	}
+	
+	public void onClick(View v) 
+	{
+		if (mParent.mExpenseDot)
+			mParent.mExpenseFrac += Integer.toString(mFigure);
+		else
+			mParent.mExpenseInt +=  Integer.toString(mFigure);
+
+		mParent.updateExpenseText();
+	}
 }
--- src/com/udesign/cashlens/CashLensActivity.java
+++ src/com/udesign/cashlens/CashLensActivity.java
@@ -2,29 +2,20 @@ package com.udesign.cashlens;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.Intent;
 import android.graphics.Color;
-import android.hardware.Camera;
-import android.hardware.Camera.Size;
 import android.os.Bundle;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
-import android.util.Log;
-import android.widget.Toast;
 
 public class CashLensActivity extends Activity {
 	private static String msgTitle1 = "No accounts found";
-	private static String msgMsg1 =  "Tap screen to create a new account";
-	private static String msgTitle2 = "Camera error";
-	private static String msgMsg2 =  "Please run on a device with camera";
+	private static String msgMsg1 = "Tap screen to create a new account";
 
 	private LinearLayout ln;
 	private Button createAccountBtn;
-	private android.hardware.Camera camera;
-	private SurfaceView cameraSurface;
-	private boolean inPreview=false;
+	private Button addExpenseBtn;
 
 	/** Called when the activity is first created. */
 	@Override
@@ -32,43 +23,28 @@ public class CashLensActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 
-		initInstance();
-	}
-
-	private void initInstance()
-	{
 		ln = (LinearLayout)findViewById(R.id.mainwindow);
 		ln.setBackgroundColor(Color.argb(0xf0, 0xf0, 0xf0, 0xf0));    	
+
 		createAccountBtn = new Button(this);
 		createAccountBtn.setText("Open Account");
-		cameraSurface = new SurfaceView(this);        
-		SurfaceHolder holder = cameraSurface.getHolder();
-		holder.addCallback(surfaceCallback);
-		holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
-
+		
+		addExpenseBtn = new Button(this);
+		addExpenseBtn.setText("Add expense");
+		
 		ln.addView(createAccountBtn);
-		ln.addView(cameraSurface);
+		ln.addView(addExpenseBtn);
+		
 		createAccountBtn.setOnClickListener(new Button.OnClickListener() { public void onClick (View v){ msg(msgTitle1, msgMsg1); }});    	
-	}
-
-	@Override
-	public void onResume() {
-		super.onResume();
-
-		camera=Camera.open();
-	}
-
-	@Override
-	public void onPause() {
-		if (inPreview) {
-			camera.stopPreview();
-		}
-
-		camera.release();
-		camera = null;
-		inPreview = false;
-
-		super.onPause();
+		addExpenseBtn.setOnClickListener(new Button.OnClickListener() 
+			{ 
+				public void onClick(View v)
+				{ 
+					Intent myIntent = new Intent(CashLensActivity.this,AddExpenseActivity.class); 
+					startActivity(myIntent); 				
+				}
+			}
+		);    	
 	}
 
 	private void msg(String title, String msg)
@@ -79,71 +55,4 @@ public class CashLensActivity extends Activity {
 		alertDialog.setMessage(msg);
 		alertDialog.show();
 	}
-
-	private Camera.Size getBestPreviewSize(int width, int height,
-			Camera.Parameters parameters) {
-		Camera.Size result = null;
-
-		for (Camera.Size size : parameters.getSupportedPreviewSizes()) {
-			if (size.width <= width && size.height <= height) {
-				if (result == null) {
-					result = size;
-				}
-				else {
-					int resultArea = result.width*result.height;
-					int newArea = size.width*size.height;
-
-					if (newArea > resultArea) {
-						result = size;
-					}
-				}
-			}
-		}
-
-		return(result);
-	}
-
-	SurfaceHolder.Callback surfaceCallback = new SurfaceHolder.Callback() {
-		public void surfaceCreated(SurfaceHolder holder) {
-			try {
-				camera.setPreviewDisplay(holder);
-			}
-			catch (Throwable t) {
-				Log.e("PreviewDemo-surfaceCallback",
-						"Exception in setPreviewDisplay()", t);
-				Toast
-				.makeText(CashLensActivity.this, t.getMessage(), Toast.LENGTH_LONG)
-				.show();
-			}
-		}
-
-		public void surfaceChanged(SurfaceHolder holder,
-				int format, int width,
-				int height) {
-			Camera.Parameters parameters = camera.getParameters();
-			if(parameters != null)
-			{
-				Camera.Size size = null;
-				if(parameters.getSupportedPreviewSizes() != null)
-					size = getBestPreviewSize(width, height,
-							parameters);
-				
-				if (size != null) {
-					parameters.setPreviewSize(size.width, size.height);
-				} else {
-					parameters.setPreviewSize(cameraSurface.getWidth(), cameraSurface.getHeight());
-				}
-				camera.setParameters(parameters);
-				camera.startPreview();
-				inPreview = true;
-
-			} else {
-				msg(msgTitle2, msgMsg2);
-			}
-		}
-
-		public void surfaceDestroyed(SurfaceHolder holder) {
-			// no-op
-		}
-	};
 }
