0dbedbc953ade729e47543a53b27ec66e9189092
Sorin Otescu <sorinu@gmail.com>
2011-11-30 20:38:06 +0200

    Using predefined layout for main activity; supports account management now.

--- src/com/udesign/cashlens/CashLensActivity.java
+++ src/com/udesign/cashlens/CashLensActivity.java
@@ -1,58 +1,72 @@
 package com.udesign.cashlens;
 
 import android.app.Activity;
-import android.app.AlertDialog;
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.LinearLayout;
 
-public class CashLensActivity extends Activity {
-	private static String msgTitle1 = "No accounts found";
-	private static String msgMsg1 = "Tap screen to create a new account";
-
-	private LinearLayout ln;
-	private Button createAccountBtn;
+public class CashLensActivity extends Activity
+{
 	private Button addExpenseBtn;
 
 	/** Called when the activity is first created. */
 	@Override
-	public void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState)
+	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 
-		ln = (LinearLayout)findViewById(R.id.mainwindow);
-		ln.setBackgroundColor(Color.argb(0xf0, 0xf0, 0xf0, 0xf0));    	
+		addExpenseBtn = (Button)findViewById(R.id.addExpense);
 
-		createAccountBtn = new Button(this);
-		createAccountBtn.setText("Open Account");
-		
-		addExpenseBtn = new Button(this);
-		addExpenseBtn.setText("Add expense");
-		
-		ln.addView(createAccountBtn);
-		ln.addView(addExpenseBtn);
-		
-		createAccountBtn.setOnClickListener(new Button.OnClickListener() { public void onClick (View v){ msg(msgTitle1, msgMsg1); }});    	
-		addExpenseBtn.setOnClickListener(new Button.OnClickListener() 
-			{ 
-				public void onClick(View v)
-				{ 
-					Intent myIntent = new Intent(CashLensActivity.this,AddExpenseActivity.class); 
-					startActivity(myIntent); 				
-				}
+		addExpenseBtn.setOnClickListener(new Button.OnClickListener()
+		{
+			public void onClick(View v)
+			{
+				Intent myIntent = new Intent(CashLensActivity.this,
+						AddExpenseActivity.class);
+				startActivity(myIntent);
 			}
-		);    	
+		});
 	}
 
-	private void msg(String title, String msg)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see android.app.Activity#onCreateOptionsMenu(android.view.Menu)
+	 */
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu)
 	{
-		AlertDialog alertDialog;
-		alertDialog = new AlertDialog.Builder(this).create();
-		alertDialog.setTitle(title);
-		alertDialog.setMessage(msg);
-		alertDialog.show();
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.cash_lens_menu, menu);
+		return true;
+	}
+
+	/* (non-Javadoc)
+	 * @see android.app.Activity#onOptionsItemSelected(android.view.MenuItem)
+	 */
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item)
+	{
+	    // Handle item selection
+	    switch (item.getItemId()) 
+	    {
+	    case R.id.manage_accounts:
+			Intent myIntent = new Intent(CashLensActivity.this,
+					AccountsActivity.class);
+			startActivity(myIntent);
+	        return true;
+	        
+	    case R.id.manage_currencies:
+	    	// TODO implement
+	        return true;
+	        
+	    default:
+	        return super.onOptionsItemSelected(item);
+	    }
 	}
 }
