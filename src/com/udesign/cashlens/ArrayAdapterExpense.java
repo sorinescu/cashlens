c3f87aa23500de5e8904f70c201a5efa969501be
Sorin Otescu <sorinu@gmail.com>
2011-12-27 23:28:48 +0200

    Added list adapter for expenses view.

---
+++ src/com/udesign/cashlens/ArrayAdapterExpense.java
@@ -0,0 +1,127 @@
+/**
+ * 
+ */
+package com.udesign.cashlens;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import com.udesign.cashlens.CashLensStorage.Expense;
+
+import android.content.Context;
+import android.text.Html;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+/**
+ * @author sorin
+ *
+ */
+public class ArrayAdapterExpense extends BaseAdapter implements ExpenseThumbnail.OnExpenseThumbnailLoadedListener
+{
+	protected ExpenseThumbnailCache mThumbCache;
+	protected ArrayList<Expense> mItems;
+	protected Context mContext;
+	protected LayoutInflater mInflater;
+	
+	public ArrayAdapterExpense(Context context, ArrayList<Expense> items)
+	{
+		mItems = items;
+		mContext = context;
+		mInflater = (LayoutInflater)mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		mThumbCache = ExpenseThumbnailCache.instance(context.getApplicationContext());
+	}
+
+	/* (non-Javadoc)
+	 * @see android.widget.Adapter#getCount()
+	 */
+	public int getCount()
+	{
+		return mItems.size();
+	}
+
+	/* (non-Javadoc)
+	 * @see android.widget.Adapter#getItem(int)
+	 */
+	public Object getItem(int position)
+	{
+		return mItems.get(position);
+	}
+
+	/* (non-Javadoc)
+	 * @see android.widget.Adapter#getItemId(int)
+	 */
+	public long getItemId(int position)
+	{
+		return mItems.get(position).id;
+	}
+
+	/* (non-Javadoc)
+	 * @see android.widget.Adapter#getView(int, android.view.View, android.view.ViewGroup)
+	 */
+	public View getView(int position, View convertView, ViewGroup parent)
+	{
+		RelativeLayout rowLayout;
+
+		if (convertView == null) 
+		{
+			Log.d("ExpenseAdapter","Creating Inflater");
+			rowLayout = (RelativeLayout)mInflater.inflate(R.layout.expense_row, parent, false);
+		} 
+		else
+			rowLayout = (RelativeLayout)convertView;
+		
+		Expense expense = mItems.get(position);
+		
+		TextView textView = (TextView)rowLayout.findViewById(android.R.id.text1);
+		
+		String html = "<b>" + expense.date.toLocaleString() + "</b>" +  "<br />";
+		if (expense.description != null)
+			html += expense.description + "<br />";
+		
+        html += expense.amountToString() + " " + expense.currencyName();
+        
+        // formatted text
+		textView.setText(Html.fromHtml(html));
+		
+		// if there's a thumbnail, override default image once thumbnail is loaded
+		if (expense.thumbnailId != 0)
+		{
+			ImageView image = (ImageView)rowLayout.findViewById(android.R.id.icon1);
+			
+			ExpenseThumbnail thumb = mThumbCache.getThumbnail(expense.thumbnailId, this, image);
+			// TODO remove self from onLoad listener of thumbnail when activity is destroyed
+			
+			try
+			{
+				image.setImageBitmap(thumb.asBitmap());
+			} catch (IOException e)
+			{
+				e.printStackTrace();
+			}
+		}
+		
+		return rowLayout;
+	}
+
+	public void onExpenseThumbnailLoaded(ExpenseThumbnail thumb, Object context)
+	{
+		ImageView image = (ImageView)context;
+	
+		Log.d("ExpenseAdapter", "thumb " + Integer.toString(thumb.id) + " loaded");
+		
+		try
+		{
+			image.setImageBitmap(thumb.asBitmap());
+		} catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
+}
