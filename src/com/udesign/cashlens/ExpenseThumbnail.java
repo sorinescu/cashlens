c69ba8142d67d9ccbeb945595ea4631a2eef7e03
Sorin Otescu <sorinu@gmail.com>
2011-12-27 23:34:09 +0200

    * renamed some members * better (?) way of generating thumbnails

--- src/com/udesign/cashlens/ExpenseThumbnail.java
+++ src/com/udesign/cashlens/ExpenseThumbnail.java
@@ -13,6 +13,9 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Rect;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.WindowManager;
 
 /**
  * @author sorin
@@ -24,9 +27,6 @@ public class ExpenseThumbnail
 	protected Bitmap mBitmap = null;
 	protected ArrayList<OnLoadedListenerCallback> mOnLoadedListeners = new ArrayList<OnLoadedListenerCallback>();
 	
-	public static final int WIDTH = 128;
-	public static final int HEIGHT = 48;
-	
 	public int id;
 	
 	public ExpenseThumbnail(Context context, int id)
@@ -35,33 +35,32 @@ public class ExpenseThumbnail
 		this.id = id;
 	}
 
-	public static interface OnLoadedListener {
-		public void OnLoaded(ExpenseThumbnail thumb, Object context);
+	public static interface OnExpenseThumbnailLoadedListener {
+		public void onExpenseThumbnailLoaded(ExpenseThumbnail thumb, Object context);
 	}
 	
 	protected static class OnLoadedListenerCallback {
-		public OnLoadedListenerCallback(OnLoadedListener listener2,
+		public OnLoadedListenerCallback(OnExpenseThumbnailLoadedListener listener2,
 				Object context2)
 		{
 			listener = listener2;
 			context = context2;
 		}
 		
-		OnLoadedListener listener;
+		OnExpenseThumbnailLoadedListener listener;
 		Object context;
 	}
 	
-	public void createFromByteArray(byte[] data) throws IOException
+	public void decodeFromByteArray(byte[] data) throws IOException
 	{
 		Bitmap thumb = BitmapFactory.decodeByteArray(data, 0, data.length);
 		if (thumb == null)
 			throw new IOException("Couldn't decode thumbnail");
 		
 		mBitmap = thumb;
-		notifyOnLoadedListeners();	// notify listeners that a new bitmap is available
 	}
 	
-	public synchronized void registerOnLoadedListener(OnLoadedListener listener, Object context)
+	public synchronized void registerOnLoadedListener(OnExpenseThumbnailLoadedListener listener, Object context)
 	{
 		// make sure we don't have duplicate entries
 		unregisterOnLoadedListener(listener, context);
@@ -69,7 +68,7 @@ public class ExpenseThumbnail
 		mOnLoadedListeners.add(new OnLoadedListenerCallback(listener, context));
 	}
 	
-	public synchronized void unregisterOnLoadedListener(OnLoadedListener listener, Object context)
+	public synchronized void unregisterOnLoadedListener(OnExpenseThumbnailLoadedListener listener, Object context)
 	{
 		for (Iterator<OnLoadedListenerCallback> iter = mOnLoadedListeners.iterator(); iter.hasNext();)
 		{
@@ -82,7 +81,10 @@ public class ExpenseThumbnail
 	public synchronized void notifyOnLoadedListeners()
 	{
 		for (OnLoadedListenerCallback callback : mOnLoadedListeners)
-			callback.listener.OnLoaded(this, callback.context);
+		{
+			Log.d("Thumbnail", "thumb id " + Integer.toString(id) + " notifying load listener " + callback.listener.toString());
+			callback.listener.onExpenseThumbnailLoaded(this, callback.context);
+		}
 	}
 
 	public Bitmap asBitmap() throws IOException
@@ -91,19 +93,25 @@ public class ExpenseThumbnail
 		return mBitmap;
 	}
 	
-	public static byte[] createFromJPEG(byte[] jpegData) throws IOException
+	public static byte[] createFromJPEG(Context context, byte[] jpegData) throws IOException
 	{
 		Bitmap jpeg = BitmapFactory.decodeByteArray(jpegData, 0, jpegData.length);
 		if (jpeg == null)
 			throw new IOException("Couldn't decode JPEG");
 		
+		DisplayMetrics metrics = new DisplayMetrics();
+		((WindowManager)context.getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay().getMetrics(metrics);
+
+		int width = Math.max(metrics.widthPixels, metrics.heightPixels);
+		int height = Math.min(metrics.widthPixels, metrics.heightPixels) / 10;
+		
 		// scale thumbnail
-		Bitmap thumb = Bitmap.createBitmap(WIDTH, HEIGHT, Bitmap.Config.ARGB_8888);
+		Bitmap thumb = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
 		Canvas canvas = new Canvas(thumb);
 		
-		float scale = Math.min(jpeg.getWidth() / WIDTH, jpeg.getHeight() / HEIGHT);
-		int realWidth = (int)(jpeg.getWidth() - scale * WIDTH);
-		int realHeight = (int)(jpeg.getHeight() - scale * HEIGHT);
+		float scale = Math.min(jpeg.getWidth() / width, jpeg.getHeight() / height);
+		int realWidth = (int)(jpeg.getWidth() - scale * width);
+		int realHeight = (int)(jpeg.getHeight() - scale * height);
 
 		Rect srcRect = new Rect();
 		srcRect.top = realHeight / 2; 
@@ -112,7 +120,7 @@ public class ExpenseThumbnail
 		srcRect.right = srcRect.left + realWidth;
 		
 		// copy the central part of the JPEG; that's where the focus should be
-		canvas.drawBitmap(jpeg, srcRect, new Rect(0, 0, WIDTH, HEIGHT), null);
+		canvas.drawBitmap(jpeg, srcRect, new Rect(0, 0, width, height), null);
 		
 		ByteArrayOutputStream out = new ByteArrayOutputStream();
 		thumb.compress(Bitmap.CompressFormat.JPEG, 50, out);
