034a16fe3d2bece78d98f2a369f6f10bccc5cec1
Sorin Otescu <sorinu@gmail.com>
2012-01-12 17:33:34 +0200

    Expense list now shows subtotals (per account and per currency) at the right of the expense.

--- src/com/udesign/cashlens/ArrayAdapterExpense.java
+++ src/com/udesign/cashlens/ArrayAdapterExpense.java
@@ -4,6 +4,8 @@
 package com.udesign.cashlens;
 
 import java.io.IOException;
+import java.text.DateFormat;
+import java.util.HashMap;
 
 import com.udesign.cashlens.CashLensStorage.Expense;
 
@@ -31,6 +33,9 @@ public class ArrayAdapterExpense extends BaseAdapter implements ExpenseThumbnail
 	protected Context mContext;
 	protected LayoutInflater mInflater;
 	protected int mOrientation;
+	protected HashMap<Integer,Integer> mTotalPerCurrency;
+	protected HashMap<Integer,Integer> mTotalPerAccount;
+	protected int mTotalPosition;	// the position up to which the total is correct
 	
 	public ArrayAdapterExpense(Context context, ArrayListWithNotify<Expense> items)
 	{
@@ -40,6 +45,10 @@ public class ArrayAdapterExpense extends BaseAdapter implements ExpenseThumbnail
 		mContext = context;
 		mInflater = (LayoutInflater)mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 		mThumbCache = ExpenseThumbnailCache.instance(context.getApplicationContext());
+		
+		mTotalPerCurrency = new HashMap<Integer,Integer>();
+		mTotalPerAccount = new HashMap<Integer,Integer>();
+		invalidateSums();
 		
 		mItems.addOnDataChangedListener(this);
 	}
@@ -85,16 +94,29 @@ public class ArrayAdapterExpense extends BaseAdapter implements ExpenseThumbnail
 		
 		Expense expense = mItems.get(position);
 		
-		TextView textView = (TextView)rowLayout.findViewById(android.R.id.text1);
+		TextView expenseText = (TextView)rowLayout.findViewById(android.R.id.text1);
+		TextView totalText = (TextView)rowLayout.findViewById(android.R.id.text2);
 		
-		String html = "<b>" + expense.date.toLocaleString() + "</b>" +  "<br />";
+		DateFormat df = DateFormat.getDateTimeInstance(DateFormat.MEDIUM, DateFormat.SHORT);
+		
+		String html = "<b>" + df.format(expense.date) + "</b>" +  "<br />";
 		if (expense.description != null)
 			html += expense.description + "<br />";
 		
-        html += expense.amountToString() + " " + expense.currencyName();
+        html += expense.amountToString() + " " + expense.currencyCode() + ", " 
+        	+ expense.accountName();
         
         // formatted text
-		textView.setText(Html.fromHtml(html));
+		expenseText.setText(Html.fromHtml(html));
+		
+		String currencyName = expense.currencyCode();
+		
+		String[] amounts = computeSum(expense, position);
+		html = expense.accountName() + ": " + amounts[1] + currencyName + "<br />" +
+			parent.getResources().getString(R.string.total) + " (" + 
+			currencyName + "):" + amounts[0] + currencyName;
+		
+		totalText.setText(Html.fromHtml(html));
 		
 		// if there's a thumbnail, override default image once thumbnail is loaded
 		if (expense.thumbnailId != 0)
@@ -134,5 +156,81 @@ public class ArrayAdapterExpense extends BaseAdapter implements ExpenseThumbnail
 	public void onDataChanged()
 	{
 		notifyDataSetChanged();
+	}
+	
+	protected Integer getTotalPerAccount(int accountId)
+	{
+		Integer key = new Integer(accountId);
+		Integer val = mTotalPerAccount.get(key);
+		if (val == null)
+		{
+			val = new Integer(0);
+			mTotalPerAccount.put(key, val);
+		}
+		
+		return val;
+	}
+	
+	protected Integer getTotalPerCurrency(int currencyId)
+	{
+		Integer key = new Integer(currencyId);
+		Integer val = mTotalPerCurrency.get(key);
+		if (val == null)
+		{
+			val = new Integer(0);
+			mTotalPerCurrency.put(key, val);
+		}
+		
+		return val;
+	}
+	
+	// Returns a string representation of the sum
+	protected String[] computeSum(Expense expenseToAdd, int position)
+	{
+		String[] res = new String[2];
+
+		Log.d("computeSum", "computing for position " + Integer.toString(position));
+		
+		// We need to recompute the sums ins this case; we can only compute forward
+		if (position < mTotalPosition)
+			invalidateSums();
+		
+		for (int i = mTotalPosition + 1; i < position; ++i)
+		{
+			Expense expense = mItems.get(i);
+			int currencyId = expense.currencyId();
+			Integer totalPerAccount = getTotalPerAccount(expense.accountId);
+			Integer totalPerCurrency = getTotalPerCurrency(currencyId);
+			
+			totalPerCurrency += expense.amount;
+			mTotalPerCurrency.put(new Integer(currencyId), totalPerCurrency);
+			
+			totalPerAccount += expense.amount;
+			mTotalPerAccount.put(new Integer(expense.accountId), totalPerAccount);
+		}
+		
+		int currencyId = expenseToAdd.currencyId();
+		Integer totalPerAccount = getTotalPerAccount(expenseToAdd.accountId);
+		Integer totalPerCurrency = getTotalPerCurrency(currencyId);
+		
+		totalPerCurrency += expenseToAdd.amount;
+		mTotalPerCurrency.put(new Integer(currencyId), totalPerCurrency);
+
+		totalPerAccount += expenseToAdd.amount;		// should also update the stored value in the map
+		mTotalPerAccount.put(new Integer(expenseToAdd.accountId), totalPerAccount);
+		
+		mTotalPosition = position;
+		
+		res[0] = Expense.amountToString(totalPerCurrency);
+		res[1] = Expense.amountToString(totalPerAccount);
+		
+		return res;
+	}
+	
+	protected void invalidateSums()
+	{
+		mTotalPerCurrency.clear();
+		mTotalPerAccount.clear();
+		mTotalPosition = -1;
 	}
 }
