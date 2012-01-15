225fd248a214019e3cebd05859e32eb6e3578280
Sorin Otescu <sorinu@gmail.com>
2012-01-15 20:51:45 +0200

    Storing expense filter.

--- src/com/udesign/cashlens/AppSettings.java
+++ src/com/udesign/cashlens/AppSettings.java
@@ -3,6 +3,13 @@
  */
 package com.udesign.cashlens;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.util.Date;
+import java.util.Locale;
+
+import com.udesign.cashlens.CashLensStorage.ExpenseFilter;
+
 import android.content.Context;
 import android.content.SharedPreferences;
 
@@ -10,22 +17,63 @@ import android.content.SharedPreferences;
  * @author sorin
  *
  */
-class AppSettings
+final class AppSettings
 {
 	private static AppSettings mInstance = null;
 	
 	private int lastUsedCurrency;
 	private int lastUsedAccount;
+	private ExpenseFilter lastUsedCustomExpenseFilter;
+	private ExpensesView.FilterType expenseFilterType;
 	
-	private SharedPreferences.Editor mSharedPrefsEditor;
+	private SharedPreferences mSharedPrefs;
 	
 	private AppSettings(Context context)
 	{
-		SharedPreferences sharedPrefs = context.getSharedPreferences("CashLensSharedPrefs", 0);
-		mSharedPrefsEditor = sharedPrefs.edit();
+		mSharedPrefs = context.getSharedPreferences("CashLensSharedPrefs", 0);
 		
-		lastUsedCurrency = sharedPrefs.getInt("lastUsedCurrency", -1);
-		lastUsedAccount = sharedPrefs.getInt("lastUsedAccount", -1);
+		lastUsedCurrency = mSharedPrefs.getInt("lastUsedCurrency", -1);
+		lastUsedAccount = mSharedPrefs.getInt("lastUsedAccount", -1);
+		
+		lastUsedCustomExpenseFilter = new ExpenseFilter();
+		lastUsedCustomExpenseFilter.startDate = getDate("lastUsedCustomExpenseFilterStart");
+		lastUsedCustomExpenseFilter.endDate = getDate("lastUsedCustomExpenseFilterEnd");
+		lastUsedCustomExpenseFilter.accountId = mSharedPrefs.getInt("lastUsedCustomExpenseFilterAccount", 0);
+		
+		int ordinal = mSharedPrefs.getInt("expenseFilterType", ExpensesView.FilterType.MONTH.ordinal());
+		expenseFilterType = ExpensesView.FilterType.values()[ordinal];
+	}
+	
+	private Date getDate(String fieldName)
+	{
+		String val = mSharedPrefs.getString(fieldName, "");
+		if (val != null)
+		{
+			DateFormat df = DateFormat.getDateInstance(DateFormat.LONG, Locale.ENGLISH);
+			try
+			{
+				Date date = df.parse(val);
+				return date;
+			} catch (ParseException e)
+			{
+				e.printStackTrace();
+				return null;
+			}
+		}
+		
+		return null;
+	}
+	
+	private void putDate(String fieldName, Date date)
+	{
+		String val = "";
+		if (date != null)
+		{
+			DateFormat df = DateFormat.getDateInstance(DateFormat.LONG, Locale.ENGLISH);
+			val = df.format(date);
+		}
+		
+		mSharedPrefs.edit().putString(fieldName, val);
 	}
 	
 	public static AppSettings instance(Context context)
@@ -49,8 +97,8 @@ class AppSettings
 	public void setLastUsedCurrency(int aLastUsedCurrency)
 	{
 		lastUsedCurrency = aLastUsedCurrency;
-		mSharedPrefsEditor.putInt("lastUsedCurrency", lastUsedCurrency);
-		mSharedPrefsEditor.commit();
+		mSharedPrefs.edit().putInt("lastUsedCurrency", lastUsedCurrency);
+		mSharedPrefs.edit().commit();
 	}
 	/**
 	 * @return the lastUsedAccount
@@ -65,8 +113,41 @@ class AppSettings
 	public void setLastUsedAccount(int aLastUsedAccount)
 	{
 		lastUsedAccount = aLastUsedAccount;
-		mSharedPrefsEditor.putInt("lastUsedAccount", lastUsedAccount);
-		mSharedPrefsEditor.commit();
+		mSharedPrefs.edit().putInt("lastUsedAccount", lastUsedAccount);
+		mSharedPrefs.edit().commit();
 	}
-	
+	/**
+	 * @return the expenseFilterType
+	 */
+	public ExpensesView.FilterType getExpenseFilterType()
+	{
+		return expenseFilterType;
+	}
+	/**
+	 * @param filterType the expenseFilterType to set
+	 */
+	public void setExpenseFilterType(ExpensesView.FilterType filterType)
+	{
+		expenseFilterType = filterType;
+		mSharedPrefs.edit().putInt("expenseFilterType", filterType.ordinal());
+		mSharedPrefs.edit().commit();
+	}
+	/**
+	 * @return the lastUsedCustomExpenseFilter
+	 */
+	public ExpenseFilter getLastUsedCustomExpenseFilter()
+	{
+		return lastUsedCustomExpenseFilter;
+	}
+	/**
+	 * @param filter the lastUsedCustomExpenseFilter to set
+	 */
+	public void setLastUsedCustomExpenseFilter(ExpenseFilter filter)
+	{
+		lastUsedCustomExpenseFilter = filter;
+		putDate("lastUsedCustomExpenseFilterStart", filter.startDate);
+		putDate("lastUsedCustomExpenseFilterEnd", filter.endDate);
+		mSharedPrefs.edit().putInt("lastUsedCustomExpenseFilterAccount", filter.accountId);
+		mSharedPrefs.edit().commit();
+	}
 }
