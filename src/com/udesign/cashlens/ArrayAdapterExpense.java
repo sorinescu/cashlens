a050803015db8cbec03c533992c8caf3ef433e4f
Sorin Otescu <sorinu@gmail.com>
2012-01-16 14:05:38 +0200

    Added Apache license.

--- src/com/udesign/cashlens/ArrayAdapterExpense.java
+++ src/com/udesign/cashlens/ArrayAdapterExpense.java
@@ -1,6 +1,18 @@
-/**
+/*******************************************************************************
+ * Copyright 2012 Sorin Otescu <sorin.otescu@gmail.com>
  * 
- */
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
 package com.udesign.cashlens;
 
 import java.io.IOException;
@@ -21,10 +33,6 @@ import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
-/**
- * @author sorin
- *
- */
 public class ArrayAdapterExpense extends BaseAdapter implements ExpenseThumbnail.OnExpenseThumbnailLoadedListener,
 	ArrayListWithNotify.OnDataChangedListener
 {
